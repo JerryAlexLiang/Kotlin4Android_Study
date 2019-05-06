d715a9104fc738d73089202f99849ea04094a2a3
1935990239@qq.com <11935990239Smile>
2019-05-06 13:44:31 +0800

    Initial commit

---
+++ app/src/main/java/liang/com/kotlin4android_widget/MainActivity.kt
@@ -0,0 +1,12 @@
+package liang.com.kotlin4android_widget
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
