b3966ac7fc56eef50d3d17be0d1162509a11d92a
1935990239@qq.com <11935990239Smile>
2019-05-08 11:22:21 +0800

    [develop] Kotlin中使用单选按钮RadioButton

---
+++ module01_simplewidget/src/main/java/liang/com/module01_simplewidget/CheckboxRadioButtonActivity.kt
@@ -0,0 +1,61 @@
+package liang.com.module01_simplewidget
+
+import android.annotation.SuppressLint
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_checkbox_radiobutton.*
+import org.jetbrains.anko.toast
+
+/**
+ * 创建日期：2019/5/7 on 20:08
+ * 描述: Kotlin - 复选框Checkbox
+ * 作者: liangyang
+ */
+class CheckboxRadioButtonActivity : AppCompatActivity() {
+
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_checkbox_radiobutton)
+
+        title = "单选按钮与复选框"
+
+//        //设置默认是未选中的状态
+//        checkBox.isChecked = false
+
+//        checkBox.setOnCheckedChangeListener { buttonView, isChecked ->
+//            toast("${if (isChecked) "勾选" else "取消勾选"}了复选框")
+//
+//            tv_checked.text = "${if (isChecked) "勾选" else "取消勾选"}了复选框"
+//        }
+
+        checkBox.setOnCheckedChangeListener { _, isChecked ->
+            toast("${if (isChecked) "勾选" else "取消勾选"}了复选框")
+
+            tv_checked.text = "${if (isChecked) "勾选" else "取消勾选"}了复选框"
+        }
+
+        radioGroup.setOnCheckedChangeListener { group, checkedId ->
+            when (checkedId) {
+                R.id.radioButtonOne -> {
+                    tvRadioSelected.text = "我最喜欢的明星是: ${radioButtonOne.text}"
+                    toast("我最喜欢的明星是: ${radioButtonOne.text}")
+                }
+                R.id.radioButtonTwo -> {
+                    tvRadioSelected.text = "我最喜欢的明星是: ${radioButtonTwo.text}"
+                    toast("我最喜欢的明星是: ${radioButtonTwo.text}")
+                }
+                R.id.radioButtonThree -> {
+                    tvRadioSelected.text = "我最喜欢的明星是: ${radioButtonThree.text}"
+                    toast("我最喜欢的明星是: ${radioButtonThree.text}")
+                }
+                R.id.radioButtonFour -> {
+                    tvRadioSelected.text = "我最喜欢的明星是: ${radioButtonFour.text}"
+                    toast("我最喜欢的明星是: ${radioButtonFour.text}")
+                }
+                else -> {
+                }
+            }
+        }
+    }
+}
--- module01_simplewidget/src/main/java/liang/com/module01_simplewidget/MainActivity.kt
+++ module01_simplewidget/src/main/java/liang/com/module01_simplewidget/MainActivity.kt
@@ -3,10 +3,11 @@ package liang.com.module01_simplewidget
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.toast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,8 +15,23 @@ class MainActivity : AppCompatActivity() {
 
         toast("Kotlin 4 Android")
 
-        btn_button.setOnClickListener { startActivity(Intent(this, ButtonActivity::class.java)) }
+//        btn_button.setOnClickListener { startActivity(Intent(this, ButtonActivity::class.java)) }
+//
+//        btn_checkbox.setOnClickListener { startActivity(Intent(this, CheckboxRadioButtonActivity::class.java)) }
 
-        btn_checkbox.setOnClickListener { startActivity(Intent(this, CheckboxActivity::class.java)) }
+        btn_button.setOnClickListener(this)
+        btn_checkbox.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when(v.id){
+            R.id.btn_button -> startActivity(Intent(this, ButtonActivity::class.java))
+
+            R.id.btn_checkbox -> startActivity(Intent(this, CheckboxRadioButtonActivity::class.java))
+
+            else -> ""
+
+
+        }
     }
 }
